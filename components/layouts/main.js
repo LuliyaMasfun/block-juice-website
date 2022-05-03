2e861af7d732c5fcd1f1eef096fc43db8a5aee24
LuliyaMasfun <luliamasfun1@gmail.com>
2022-05-04 00:33:05 +0200

    integrate with chakra UI

---
+++ components/layouts/main.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import {Box, Container} from '@chakra-ui/react'
+
+
+
+const Main = ({children, router}) => {
+    return(
+        <Box as="main" pb={8}>
+            <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Block Juice homepage" />
+                <meta name="author" content="Luliya Masfun" />
+                <meta name="author" content="Block Juice" />
+
+                <title>Block Juice - Homepage</title>
+            </Head>
+
+            <Container maxW="container.md" pt={14}>
+                {children}
+            </Container>
+        </Box>
+    )
+}
+export default Main
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,29 @@
-const Page = ( ) =>{
-    return(
-        <div>Hello</div>
-    )
+import { Container, Box, Heading } from '@chakra-ui/react'
+
+const Page = () => {
+  return (
+    <Container>
+      <Box
+        borderRadius="lg"
+        p={3} 
+        mb={6} 
+        textAlign="center"
+        
+        >
+        Hello, we are video creators based in Sweden!
+      </Box>
+
+      <Box display={{ md: 'flex' }}>
+        <Box flexGrow={1}>
+        <Heading as="h2" variant="page-title">
+            Block Juice
+          </Heading>
+        <p> Block Juice is a production company and plattform responsible for creating and distributing some of Swedens most prominent music videos. We have the ambition to create commercial projects, music videos and documentaries that communicate with their audience at moment of interaction. Block Juice's visuals create feeling and visability for your products and brands at the moment of interaction. 
+
+        </p>
+      </Box>
+      </Box>
+    </Container>
+  )
 }
 export default Page
