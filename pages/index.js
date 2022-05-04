6cd000b80999e29f961d83a0059e683fe05af20b
LuliyaMasfun <luliamasfun1@gmail.com>
2022-05-04 15:51:14 +0200

    index.js almost done, add portfolio page

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,68 @@
-import { Container, Box, Heading } from '@chakra-ui/react'
-
+import NextLink from 'next/link'
+import {
+    Container,
+    Box,
+    Heading,
+    Button,
+    Link,
+    List,
+    ListItem
+     } from '@chakra-ui/react'
+import Section from'../components/section'
+import {ChevronRightIcon} from '@chakra-ui/icons'
+import { IoLogoYoutube, IoLogoInstagram } from 'react-icons/io5'
 const Page = () => {
   return (
     <Container>
+        <Section delay={0.1}>
       <Box
         borderRadius="lg"
         p={3} 
         mb={6} 
         textAlign="center"
         >
-        Hello, we are video creators based in Sweden!
+        Hello, we are a production company based in Sweden!
       </Box>
+      </Section>
 
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-        <Heading as="h2" variant="page-title">
-            Block Juice
-          </Heading>
-        <p> Block Juice is a production company and plattform responsible for creating and distributing some of 
-            Swedens most prominent music videos. We have the ambition to create commercial projects, music videos and documentaries that communicate with their audience at moment of interaction.
-            Block Juice's visuals create feeling and visability for your products and brands at the moment of interaction.
-        </p>
-      </Box>
-      </Box>
+      <Section delay={0.1}>
+      <Box align="center" my={4}>
+          <NextLink href="/portfolio" scroll={false}>
+            <Button rightIcon={ <ChevronRightIcon /> } colorScheme="orange">
+              The Portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+
+      <Section delay={0.3}>
+      <List>
+      <ListItem>
+            <Link href="https://instagram.com/blockjuice.ent" target="_blank">
+              <Button
+               fontSize={15}
+                variant="ghost"
+                colorScheme="orange"
+                leftIcon={<IoLogoInstagram />}
+              >
+                @blockjuice.ent
+              </Button>
+            </Link>
+          </ListItem>
+          <ListItem>
+            <Link href="https://youtube.com/blockjuice" target="_blank">
+              <Button
+                fontSize={15}
+                variant="ghost"
+                colorScheme="orange"
+                leftIcon={<IoLogoYoutube />}
+              >
+                Block Juice
+              </Button>
+            </Link>
+          </ListItem>
+        </List>
+      </Section>
     </Container>
   )
 }
---
+++ pages/portfolio.js
@@ -0,0 +1,20 @@
+import {
+    Container,
+    Box,
+    Heading,
+    SimpleGrid,
+    Divider
+} from '@chakra-ui/react'
+import Section from '../components/section'
+
+const Portfolio = () =>{
+    return(
+        <Container>
+            <Heading as="h2" fontSize={20} mb={4}>
+            Portfolio
+            </Heading>
+        </Container>
+    )
+}
+
+export default Portfolio
