28ca3fbac9950e515c7bd6e4581342b056fd622a
Luliya Masfun <luliyamasfun@MacBook-Pro-som-tillhor-Luliya.local>
2022-05-28 18:20:23 +0200

    fix deploy bugs

--- pages/portfolio/1cuz-rakna-dagar.js
+++ pages/portfolio/1cuz-rakna-dagar.js
@@ -1,4 +1,4 @@
-import { Container, Badge, Link, List, NextLink, ListItem, SimpleGrid, GridItem, Divider } from '@chakra-ui/react'
+import { Container, Badge, Link, List, ListItem, SimpleGrid, GridItem, Divider } from '@chakra-ui/react'
 import { Title, PortfolioImage, Meta } from '../../components/portfolio'
 import P from '../../components/paragraph'
 import Layout from '../../components/layouts/article'
